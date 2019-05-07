aeab971559c706b1cbb6fb3563d445a3bbdd6478
[DEV] Tesla <dev@fairytail.eu>
2019-05-07 22:47:24 +0300

    Improved exceptions handling, read delay and add tags links collecting.

--- urlscollector.py
+++ urlscollector.py
@@ -3,6 +3,7 @@ from time import sleep
 from threading import Thread
 from grab import Grab
 from multiprocessing import Queue
+from tagsdatacollector import Tag
 
 
 log = logging.getLogger("PScrapper")
@@ -16,22 +17,27 @@ class URLsCollector(Thread):
         self._categories = conf.catconf["categories"]
         self._noskip = conf.catconf["noskip"]
 
-        self._sleeptime = 1
+        self._sleeptime = 0.8
 
         self._web = Grab(timeout=70, connect_timeout=70, user_agent="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0")
 
         self.urlqueue = Queue()
+        self.tagsqueue = Queue()
 
     def run(self):
         log.info("Starting URL's collector.")
 
-        if self._login():
-            for url in self._categories:
-                results = self._getMenuSubURLs(url)
+        try:
+            if self._login():
+                for url in self._categories:
+                    results = self._getMenuSubURLs(url)
 
-                if results is not None:
-                    for result in results:
-                        self.urlqueue.put(result)
+                    if results is not None:
+                        for result in results:
+                            self.urlqueue.put(result)
+
+        except Exception as ex:
+            log.error(ex)
 
         log.info("Exiting URL's collector.")
 
@@ -70,7 +76,7 @@ class URLsCollector(Thread):
                 for result in results:
                     # If not in translations config part ???
                     if result.split("?")[0] not in self._noskip:
-                        # log.debug("Skip URL: {}.".format(result))
+                        log.debug("Skip URL: {}.".format(result))
                         continue
 
                     self.urlqueue.put(self._conf["url"] + result)
@@ -80,12 +86,33 @@ class URLsCollector(Thread):
         else:
             maxpage = 1
 
-        if len(listt) == 0:
-            for page in range(1, int(maxpage) + 1):
-                # if "tver/fladskaerm" in url:
-                #     urls.append(url + "?page=" + str(page) + "&tb[stock]=allproducts")
-                # else:
-                urls.append(url + "?tb[limit]=96&page=" + str(page))
+        if url not in self._noskip:
+            log.info("Skip URL: {}.".format(url))
+        else:
+            if len(listt) == 0:
+                for page in range(1, int(maxpage) + 1):
+                    # if "tver/fladskaerm" in url:
+                    #     urls.append(url + "?page=" + str(page) + "&tb[stock]=allproducts")
+                    # else:
+                    urls.append(url + "?tb[limit]=96&page=" + str(page))
+
+            log.debug("Loded URL: {}".format(url))
+
+            for name in self._web.doc("//div[@class='check']"):
+                tag = Tag()
+                tag.groupurl = url
+                tag.name = name.select("./h4").text()
+
+                for t in name.select("./ul[@class='list-unstyled']/li/span/label"):
+                    value = t.select("input/@data-value").text()
+                    filter = t.select("input/@data-filter").text()
+                    type = t.select("input/@data-type").text()
+                    name = t.select("span").text()
+
+                    tag.values.append([value, filter, type, name])
+
+                log.debug("Tags URL: {} -> {}".format(url, tag.name))
+                self.tagsqueue.put(tag)
 
         if len(urls) is not 0:
             return urls
